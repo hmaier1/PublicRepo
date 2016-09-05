752f1b8903a4e8bcf817c2d511427f6f651660fb
Hannah Maier <hmmaier97@gmail.com>
2016-09-05 13:33:04 -0400

    Official Update Sep 5

---
+++ Game1-hMaier/Assets/Scripts/PickUpRotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpRotator : MonoBehaviour {
+
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (0.0f,30, 0.0f) * Time.deltaTime * 2);
+	}
+}
